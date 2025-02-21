0b0bdba65f8840258fa89e5923febbba751a4382
Rida <khanunzila42@gmail.com>
2025-02-21 18:25:19 +0500

    updated requirements.txt

--- controller.py
+++ controller.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request
-
 app = Flask(__name__)
 
 @app.route('/quiz')
 def quiz():
     return render_template('quiz.html')
-
 @app.route('/')
 def Home():
     return render_template('register.html')
